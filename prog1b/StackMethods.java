ccb87fa6f7396f98eaefeb8b6d04921114b522a4
Euno257 <harshghlt25@gmail.com>
2019-05-18 14:48:02 +0530

    final commit

--- prog1b/StackMethods.java
+++ prog1b/StackMethods.java
@@ -14,7 +14,7 @@ class StackMethods
   {
     if(top==size-1)
     {
-      system.out.println("Stack is full, can't push a value");
+      System.out.println("Stack is full, can't push a value");
     }
     else
     {
@@ -24,6 +24,7 @@ class StackMethods
   }
   public int pop()
   {
+    int t;
     if(top==-1)
     {
       System.out.println("Stack is empty");
@@ -31,8 +32,50 @@ class StackMethods
     }
     else
     {
-      top--;
-      return stack[top--];
+      t= top--;
+      return stack[t];
     }
+  }
+  public void display()
+  {
+    for(int i=top;i>-1;i--)
+    {
+        System.out.print(stack[i]+" ");
+    }
+    System.out.println("\n");
+  }
+
+  public static void main(String args[])
+  {
+    Scanner in= new Scanner(System.in);
+    System.out.println("Enter the Size of Stack\n");
+    int n = in.nextInt();
+    int choice;
+    StackMethods stk = new StackMethods(n);
+    do
+    {
+      System.out.println("\nStack operations");
+      System.out.println("1.Push");
+      System.out.println("2. Pop");
+      System.out.println("3. display");
+      System.out.println("\nEnter your Choice");
+      int ch = in.nextInt();
+      switch(ch)
+      {
+        case 1: System.out.println("\nEnter the element to push");
+                stk.push(in.nextInt());
+                break;
+        case 2: int s = stk.pop();
+                if(s!=-1)
+                System.out.println("Popped element="+s);
+                break;
+        case 3: stk.display();
+                break;
+      }
+      System.out.println("\nPress 1 to continue");
+      choice = in.nextInt();
+    }
+
+    while(choice==1);
   }
 }
