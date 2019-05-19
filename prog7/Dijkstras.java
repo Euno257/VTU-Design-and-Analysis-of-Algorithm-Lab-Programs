316b6926549d065d3659336dc226a1182fa947ef
Euno257 <harshghlt25@gmail.com>
2019-05-20 00:36:28 +0530

    prog6a and prog6b not completed

---
+++ prog7/Dijkstras.java
@@ -0,0 +1,14 @@
+import java.java.util.*;
+public class Dijkstras
+{
+  final static int infinity = 9999;
+  final static int MAX = 20;
+  static int a[][];
+  static int n;
+  static Scanner in = new Scanner(System.in);
+
+  public static void main(String args[])
+  {
+    a = new int[MAX][MAX]
+  }
+}
